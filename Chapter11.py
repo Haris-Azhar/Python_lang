12810a89992eab68741c4ae1b3604f12387858e4
Haris Azhar <opdenish074@gmail.com>
2026-07-18 21:48:57 +0500

    first

--- Chapter11.py
+++ Chapter11.py
@@ -93,6 +93,94 @@ class Dog(Pets):
 
 c = Dog()
 c.bark()
+<<<<<<< HEAD
 
 class employee:
     pass
+=======
+
+class complex:
+    def __init__(self,r,i):
+        self.r = r
+        self.i = i
+    def __add__(self,c2):
+        return complex(self.r + c2.r, self.i +  self.i)
+    def __mul__(self,c2):
+        return complex(self.r * c2.r, self.i *  self.i)
+
+    def __str__(self):
+        return f'{self.r} + {self.i}i'
+
+c1 = complex(5,7)
+c2 = complex(2,4)
+print(c1 +c2)
+print(c1 *c2)
+
+# Find the len of vector
+class vector:
+    def __init__(self,lis):
+        self.lis = lis
+
+    def __len__(self):
+        return len(self.lis)
+
+v1 = vector([3,4,6,7,5,7,9])
+print(len(v1))
+
+class vector2:
+    def __init__(self,a,b,c):
+        self.a = a 
+        self.b = b
+        self.c = c
+    def __add__(self,var):
+        work = vector2(self.a + var.a, self.b + var.b ,self.c + var.c)
+        return work
+
+    def __mul__(self,var):
+        return vector2(self.a * var.a, self.b * var.b ,self.c * var.c)
+
+    def __str__(self):
+        return f'{self.a}i + {self.b}j + {self.c}k'
+
+v3 = vector2(1,3,4)
+v4 = vector2(5,7,9)
+v5 = vector2(6,1,2)
+
+print(v3 + v4 + v5)
+print(v3 * v4 * v5)
+
+
+
+class vector2:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def __add__(self, var):
+        return vector2(
+            self.a + var.a,
+            self.b + var.b,
+            self.c + var.c
+        )
+
+    def __mul__(self, var):
+        return vector2(
+            self.a * var.a,
+            self.b * var.b,
+            self.c * var.c
+        )
+
+    def __str__(self):
+        return f'{self.a}i + {self.b}j + {self.c}k'
+
+
+v3 = vector2(1, 3, 4)
+v4 = vector2(5, 7, 9)
+v5 = vector2(6, 1, 2)
+
+print(v3 + v4 + v5)
+print(v3 * v4 * v5)
+
+
+>>>>>>> a97a365 (Updated chapter11.py)
